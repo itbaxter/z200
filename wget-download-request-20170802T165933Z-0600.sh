3d54422ce868c347a48acafbffbb8dc7a317557a
Ian Baxter <itbaxter@ucsb.edu>
2017-08-02 19:01:58 -0400

    added wget script

---
+++ wget-download-request-20170802T165933Z-0600.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+wget -O 'b.e11.B1850C5CN.f19_g16.0850cntl.001.cam.h0.T850.085001-184912.nc' 'http://www.earthsystemgrid.org/archive/request/b638fcb1-3030-4608-975f-cbc45ffe9439/file/abc3f029-4afb-4a04-82c6-dedd677381a4?authzToken=497b2b8a-1f68-4516-9a22-36372df7efe7'
+wget -O 'b.e11.B1850C5CN.f19_g16.0850cntl.001.cam.h0.T850.185001-200512.nc' 'http://www.earthsystemgrid.org/archive/request/b638fcb1-3030-4608-975f-cbc45ffe9439/file/5b179267-43b9-4b84-a482-5da4880b6e94?authzToken=c5f0cd67-7d5c-4d3c-a624-6c314a87b10e'
